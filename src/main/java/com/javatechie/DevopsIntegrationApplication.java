01bb308773f37e4a001ba0976a4230621f454b5a
Abhigit90 <118679266+Abhigit90@users.noreply.github.com>
2025-12-26 23:07:05 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to Devops";
+		return "Welcome to my Chauhan Hotel";
 	}
 
 	public static void main(String[] args) {
